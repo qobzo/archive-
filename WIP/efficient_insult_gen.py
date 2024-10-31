089b374dea7f211b27ebf308e73aeae9514371e5
hailey <qobzoid@gmail.com>
2024-10-30 17:56:06 -0700

    Create efficient_insult_gen.py
    Insult generator, can't remember which book this is from - Automating the Boring Stuff?

---
+++ WIP/efficient_insult_gen.py
@@ -0,0 +1,11 @@
+from random import choice
+
+adjectives = ["wet", "big"]
+nouns = ["turnip", "dog"]
+
+print("You are a ")
+
+adjective = choice(adjectives)
+
+print(adjectives)
+print(choice(nouns))
