1fe06a1da6fcedca45c79e7b8950729b137a8e9a
hailey <qobzoid@gmail.com>
2024-10-31 04:40:46 -0700

    Update to_wear_a_raincoat.py
    Accidentally broke it

--- to_wear_a_raincoat.py
+++ to_wear_a_raincoat.py
@@ -1,24 +1,24 @@
-# Rewrote it Oct. 30th, 2024
 def define(a, b):
     while True:
         try:
             globals()[a] = b
             break
         except ValueError:
-            print("Type in a number between 0-100.")
+            print("Type in a number (ex. 1, 50, 100).")
 
 while True:
     define("location", input("Where do you live?\n"))
     if location.lower() == "portland":
         location = input("Portland, OR?\n").lower()
-        if location.startswith(("y", "or")) or location == "portland, or":
-            print("Portland, OR?! You better bring that raincoat!")
-            quit()
-        else:
-            define("chance", int(input("What % likelihood is there of rain?\n")))
-            if chance > 50:
-                print("I'd wear a raincoat")
-                break
-            else: 
-                print("Leave that raincoat at home.")
-                break
+    if location.startswith(("y", "or")) or location == "portland, or":
+        location = "portland, or"
+        print("Portland, OR?! You better bring that raincoat!")
+        quit()
+    else:
+        define("chance", int(input("What % likelihood is there of rain?\n")))
+        if chance > 50:
+            print("I'd wear a raincoat")
+            break
+        else: 
+            print("Leave that raincoat at home.")
+            break
