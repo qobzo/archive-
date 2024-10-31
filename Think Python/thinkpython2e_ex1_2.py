e590f1502ce1384a215868cfcadcbb036b65d3df
hailey <qobzoid@gmail.com>
2024-10-30 17:53:24 -0700

    Create thinkpython2e_ex1_2.py
    Chapter 1, exercise 2 from Think Python, 2nd ed.

---
+++ Think Python/thinkpython2e_ex1_2.py
@@ -0,0 +1,15 @@
+import datetime
+
+# Assignment 1: How many seconds are there in 42 minutes, 42 seconds?
+print(42 * 60 + 20)
+
+# Assignment 2: How many miles are there in 10 kilometers?
+print(10 / 1.61)
+
+# Assignment 3: If you run a 10 kilometer race in 42 minutes, 42 seconds, what is your average pace?
+pace_in_sec = 2562 / 6.2
+print(datetime.timedelta(seconds=pace_in_sec))
+
+# Assignment 4: What is your average speed in miles per hour?
+minutes = 42 + 42/60
+print(6.2/minutes)
