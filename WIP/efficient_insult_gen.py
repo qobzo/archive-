e33a9fe6f6fba905491afa6d7ea6fdb2fc9c15c9
hailey <qobzoid@gmail.com>
2024-10-30 17:57:04 -0700

    Update efficient_insult_gen.py
    An attempt at a more efficient insult generator (2021).

--- WIP/efficient_insult_gen.py
+++ WIP/efficient_insult_gen.py
@@ -1,11 +1,8 @@
+# Current version (2021)
 from random import choice
 
 adjectives = ["wet", "big"]
 nouns = ["turnip", "dog"]
+insult = choice(adjectives + nouns)
 
-print("You are a ")
-
-adjective = choice(adjectives)
-
-print(adjectives)
-print(choice(nouns))
+print(f"You are a {insult}")
