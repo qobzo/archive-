9dba28b9640dfc39900ad372090609732459429c
hailey <qobzoid@gmail.com>
2024-10-31 09:52:50 -0700

    Update to_wear_a_raincoat.py

--- to_wear_a_raincoat.py
+++ to_wear_a_raincoat.py
@@ -1,9 +1,15 @@
+def define(a, b):
+    if isinstance(b, int):
+        globals()[a] = int(b)
+    else:
+        globals()[a] = b.lower()
+
 while True:
-    location = input("Where do you live?\n")
-    if location.lower() == "portland":
+    define("location", input("Where do you live?\n"))
+    if location == "portland":
         while True:
             location = input("Portland, OR?\n")
-            if location.startswith(("y", "or", "portland, or")):
+            if location == "portland, or" or location.startswith(("y", "or", "portland, o")):
                 location = "portland, or"
                 break
             elif location.startswith("n"):
@@ -12,17 +18,15 @@ while True:
 
 while True:
     if location == "portland, or":
-        print("Portland?! You better bring a raincoat!")
-        quit()
-    else:
-        chance = input("What % likelihood is there of rain?\n")
-        try:
-            chance = int(chance)
-            if chance > 50:
-                print("I'd wear a raincoat")
-                break
-            else:
-                print("Leave that raincoat at home.")
-                break
-        except ValueError:
-            print("Type in a number from 1-100.")
+        print("Portland, OR?! You better bring that raincoat!")
+        quit()        
+    try:
+        define("chance", int(input("What % likelihood is there of rain?\n")))
+        if chance > 50:
+            print("I'd wear a raincoat")
+            break
+        else: 
+            print("Leave that raincoat at home.")
+            break
+    except ValueError:
+        print("Type in a number from 1-100.")
