ccb94cde08b524838202b13e4c6bbcb382f1dfcb
hailey <qobzoid@gmail.com>
2024-10-30 18:20:40 -0700

    Update idiot.py
    Rewrote it back in Oct 2022 using regex.

--- Automate the Boring Stuff/idiot.py
+++ Automate the Boring Stuff/idiot.py
@@ -1,10 +1,4 @@
-import pyinputplus as pyip
-import re
-
 while True:
-    prompt = 'Want to know how to keep an idiot busy for hours?\n'
-    response = pyip.inputYesNo(prompt)
-    if response == 'no':
+    if re.compile(r'N.*', re.IGNORECASE).findall(input('Want to know how to keep an idiot busy for hours?\n')):
+        print('Thank you. Have a nice day.\n')
         break
-
-print('Thank you. Have a nice day.\n')
